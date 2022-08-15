f0f849bb61f647c99073b5636dafb8a62027bd7e
b <bitprofessor@163.com>
2022-08-15 12:09:40 +0800

    first

---
+++ examples/btc_quick_start.py
@@ -0,0 +1,6 @@
+import os
+import pandas as pd
+pd.set_option('mode.chained_assignment', None)
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 20)
+os.environ['czsc_verbose'] = "1"        # 是否输出详细执行信息，0 不输出，1 输出
